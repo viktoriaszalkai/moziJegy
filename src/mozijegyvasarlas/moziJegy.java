df02d4668a6c9fbbb4aa692a4d4d579e3cb5b985
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2023-11-14 13:35:25 +0100

    atnevezett projekt

--- src/mozijegyvasarlas/moziJegy.java
+++ src/mozijegyvasarlas/moziJegy.java
@@ -8,12 +8,12 @@ package mozijegyvasarlas;
  *
  * @author vikis
  */
-public class moziJegy extends javax.swing.JFrame {
+public class MoziJegy extends javax.swing.JFrame {
 
     /**
      * Creates new form moziJegy
      */
-    public moziJegy() {
+    public MoziJegy() {
         initComponents();
     }
 
@@ -30,7 +30,7 @@ public class moziJegy extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("ures projekt");
+        jLabel1.setText("atnevezett projekt");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -39,7 +39,7 @@ public class moziJegy extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(152, 152, 152)
                 .addComponent(jLabel1)
-                .addContainerGap(178, Short.MAX_VALUE))
+                .addContainerGap(144, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -69,20 +69,21 @@ public class moziJegy extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(moziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MoziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(moziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MoziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(moziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MoziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(moziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MoziJegy.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new moziJegy().setVisible(true);
+                new MoziJegy().setVisible(true);
             }
         });
     }
